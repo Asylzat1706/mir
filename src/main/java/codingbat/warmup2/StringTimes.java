f345154876fe72710c70618f31c3060ac6eb4da0
Aruuzat <hanbiyketemirlan@gmail.com>
2024-08-15 14:38:41 +0300

    resalt

---
+++ src/main/java/codingbat/warmup2/StringTimes.java
@@ -0,0 +1,12 @@
+package codingbat.warmup2;
+
+public class StringTimes {
+    public String stringTimes(String str, int n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            result.append(str);
+        }
+        return result.toString();
+    }
+
+}
